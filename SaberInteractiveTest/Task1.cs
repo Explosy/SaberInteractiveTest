9270b7b900a2f1e6872bfbfd30b3dd11dac0b9d6
Explosy <moroza.exe@yandex.ru>
2022-09-09 16:16:29 +0300

    add Task1 and Task2 class

---
+++ SaberInteractiveTest/Task1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaberInteractiveTest
+{
+    static internal class Task1
+    {
+        static string GetBinaryNumber(int decNumber)
+        {
+            return "";
+        }
+    }
+}
---
+++ SaberInteractiveTest/Task2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaberInteractiveTest
+{
+    static internal class Task2
+    {
+        static string RemoveDuplicates(string text)
+        {
+            return "";
+        }
+    }
+}
