ae6d961df6c992cedff55faacc64cdec523b62b0
Explosy <moroza.exe@yandex.ru>
2022-09-10 17:37:28 +0300

    Add realization RemoveDuplicates with StringBuilder. Also add test for this

--- SaberInteractiveTest/Task2.cs
+++ SaberInteractiveTest/Task2.cs
@@ -1,4 +1,6 @@
-﻿namespace SaberInteractiveTest
+﻿using System.Text;
+
+namespace SaberInteractiveTest
 {
     /// <summary>
     /// Класс реализующий функцию удаления дубликатов символов из строки.
@@ -26,6 +28,23 @@
             }
 
             return new string(charWithoutDuplicates, 0, charCount+1);
+        }
+        static public string RemoveDuplicates(StringBuilder text)
+        {
+            char[] charWithoutDuplicates = new char[text.Length];
+            int charCount = 0;
+            int index = 0;
+
+            charWithoutDuplicates[charCount] = text[index]; //Добавление первого символа строки в результирующий массив символов
+            index++;
+
+            for (; index < text.Length; index++) //Обработка оставшихся символов
+            {
+                if (text[index] == charWithoutDuplicates[charCount]) continue;
+                charWithoutDuplicates[++charCount] = text[index];
+            }
+
+            return new string(charWithoutDuplicates, 0, charCount + 1);
         }
     }
 }
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SaberInteractiveTest;
 
@@ -29,6 +30,17 @@ namespace Tests
             string actualResult; //Актуальный результат
 
             actualResult = Task2.RemoveDuplicates("AAA BBB  AAA CC FD");
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_RemoveDuplicatesFunction2_Task2()
+        {
+            string expectedResult = "A B A C FD"; //Ожидаемый результат
+            string actualResult; //Актуальный результат
+            StringBuilder stringBuilder = new StringBuilder("AAA BBB  AAA CC FD");
+
+            actualResult = Task2.RemoveDuplicates(stringBuilder);
             Assert.AreEqual(expectedResult, actualResult);
         }
     }
